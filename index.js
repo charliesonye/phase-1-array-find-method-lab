a8d370075aff07e38835918bc2a10ae3c11a9187
Charles <c_onyemem@live.com>
2021-11-05 16:30:21 -0500

    commit tree 191765471d1244e7ce736072eed5a5e7cfec7e9e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function superbowlWin(record) {
 
+  const wFinder = record.find(record => record.result === "W")
+  if(wFinder) {
+      return wFinder.year
+  }
+  else if(!wFinder) {
+      return undefined
+  }
+}
